c03ce1ca92f63e77e6ac49a03a552c72174d954d
Hikmatul-Maulidiyah <70553004+Hikmatul-Maulidiyah@users.noreply.github.com>
2020-12-11 21:52:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var n = prompt("Masukan Nama Anda:");
 var c = confirm("Hai " + n + " Selamat Datang di Home Bakery");
 if (c == true) {
-    alert('Selamat Berbelanja');
+    alert('Selamat Berbelanja♡');
 } else {
     alert('No Problem');
 }
